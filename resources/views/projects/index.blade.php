1d627d4f7271fb6018621d2b94e7e7e595b2a353
Helder Jordão <hjordao@hotmail.com>
2018-12-13 10:35:57 +0000

    LARAVEL Initial commit

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Title</title>
+</head>
+<body>
+	<h1>Projects Main Page:</h1>
+
+	<ul>
+	@foreach ($projects as $project)
+
+		<li>{{ $project->title }}</li>
+
+	@endforeach
+	</ul>
+</body>
+</html>
